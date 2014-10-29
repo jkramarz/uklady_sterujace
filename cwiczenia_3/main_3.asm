d7e42d392072e695e916d900d06448232dc959ed
Jakub Kramarz <lenwe@lenwe.net>
2014-10-29 17:39:37 +0100

    cwiczenia 3, zadanie 3

---
+++ cwiczenia_3/main_3.asm
@@ -0,0 +1,11 @@
+--2014-10-29 17:38:32--  http://wklej.to/gstky/text
+Resolving wklej.to (wklej.to)... 144.76.40.219
+Connecting to wklej.to (wklej.to)|144.76.40.219|:80... connected.
+HTTP request sent, awaiting response... 200 OK
+Length: 1859 (1,8K) [text/plain]
+Saving to: ‘text’
+
+     0K .                                                     100%  297M=0s
+
+2014-10-29 17:38:32 (297 MB/s) - ‘text’ saved [1859/1859]
+
